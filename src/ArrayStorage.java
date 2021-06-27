7cb5d962268c22f45af5725efc2f0c6b1af940c2
jkEvgeny <jk.galchenko@yandex.ru>
2021-06-27 19:23:44 +0300

    HW0 Test exercise. Add ArrayStorage. Add methods: clear, save, get, delete, getAll and size.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,59 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int index = 0;
 
     void clear() {
+        Arrays.fill(storage, 0, index, null);
+        index = 0;
     }
 
     void save(Resume r) {
+        storage[index] = r;
+        index++;
     }
 
     Resume get(String uuid) {
+        for (int k = 0; k < index; k++) {
+            if (storage[k].toString().equals(uuid)) {
+                return storage[k];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        int iUuid = 0;
+        for (int k = 0; k < index; k++) {
+            if (storage[k].toString().equals(uuid)) {
+                iUuid = k;
+            }
+        }
+
+        for (int i = 0; i < storage.length; i++) {
+            if (i == iUuid) {
+                // shifting elements
+                if (storage.length - 1 - i >= 0) System.arraycopy(storage, i + 1, storage, i, storage.length - 1 - i);
+                break;
+            }
+        }
+        index--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, index);
     }
 
     int size() {
-        return 0;
+        return index;
     }
 }
